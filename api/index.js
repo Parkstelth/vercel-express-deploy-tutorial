e3ace1ddc8b801c37849bc1aa3e73a5e9cadef76
sssssqew <sunrise.in.my.dream@gmail.com>
2024-05-23 09:55:40 +0900

    몽고 아틀라스 연동 2

--- api/index.js
+++ api/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose")
 const app = express();
 
-console.log("주소:", process.env.MONGODB_URI)
-mongoose.connect("mongodb+srv://sunrise:you1234@cluster0.q2zkcuy.mongodb.net/sunrise?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("데이터베이스 연결 성공 !"))
 .catch(e => console.log(`데이터베이스 연결 실패 ${e}`))
 
