19f6d513e4cad23cb0fa6d92b54d3607428d38c2
sssssqew <sunrise.in.my.dream@gmail.com>
2024-05-23 12:28:53 +0900

    회원가입 로직 추가 5

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const corsOptions = {
 
 console.log("몽고아틀라스 주소: ", process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("데이터베이스 연결 성공 !!"))
+.then(() => console.log("데이터베이스 연결 성공 !!!"))
 .catch(e => console.log(`데이터베이스 연결 실패 ${e}`))
 
 app.use(cors(corsOptions)) // cors 설정 미들웨어 
